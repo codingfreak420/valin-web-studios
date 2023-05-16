1ebd52ffcf7d8dc57b354653f439d0e2bbbc1e66
codingfreak420 <131491154+codingfreak420@users.noreply.github.com>
2023-05-16 10:36:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -20,3 +20,98 @@ function adjustTextSize() {
 }
 
 window.addEventListener('resize', adjustTextSize);
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+//const scrubValue = true;
+const scrubValue = 0.5;
+
+let container = document.querySelector('.container')
+
+// const scrollBar = gsap.to('.scrollbar', { x: () => { return window.innerWidth - (150 + 20) }, ease: "none" })
+
+ScrollTrigger.create({
+    trigger: ".container",
+    start: "top top",
+    // end: () => (container.scrollWidth - window.innerWidth),
+    end: "+=1000",
+    pin: true,
+    anticipatePin: 1,
+    scrub: scrubValue,
+    // animation: scrollBar,
+    invalidateOnRefresh: true,
+})
+
+
+
+let thumbNails = gsap.utils.toArray(".thumbnail");
+
+thumbNails.forEach((thumb, i) => {
+ 
+  if (thumb.classList.contains('fakePin')) {
+                
+    function prevAll(element) {
+      var result = [];
+
+      while (element = element.previousElementSibling)
+          result.push(element);
+      return result;
+    }    
+    
+    // console.log(prevAll(thumb))
+    
+    var totalWidthToMove;
+    
+    function getTotalWidthToMove() {
+        
+      totalWidthToMove = 0;
+      
+      prevAll(thumb).forEach((thumbBefore, i) => {
+
+        let style = thumbBefore.currentStyle || window.getComputedStyle(thumbBefore);      
+        let marginRight = parseInt(style.marginRight);
+
+        totalWidthToMove += thumbBefore.offsetWidth - 100;
+        console.log(totalWidthToMove);
+
+      });    
+
+      return totalWidthToMove;
+      
+    }
+    //getTotalWidthToMove();
+    // console.log(totalWidthToMove);
+    
+    gsap.to(thumb, {
+      x: () => { return - getTotalWidthToMove() },
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".wrapper",
+        start: 'top top',
+        scrub: scrubValue,
+        invalidateOnRefresh: true,
+        end: () => "+=" + getTotalWidthToMove(),
+      }
+    });
+    
+  }
+  else {
+    
+    gsap.to(thumb, {
+      x: () => { return - (container.scrollWidth) },
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".wrapper",
+        start: 'top top',
+        scrub: scrubValue,
+        invalidateOnRefresh: true,
+        end: () => "+=" + (container.scrollWidth),
+      }
+    });
+    
+  }
+    
+  //console.log(thumb.offsetWidth)
+  
+});
