fa95c55b72eddd172d8dfd87cb8c0d543d7bce45
codingfreak420 <131491154+codingfreak420@users.noreply.github.com>
2023-05-17 12:53:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 function adjustTextSize() {
-  const div = document.getElementById('myDiv');
-  const text = document.getElementById('myText');
-  const maxHeight = div.clientHeight;
-  
-  let fontSize = 100; // Initial font size
-  text.style.fontSize = fontSize + 'px';
-  
-  while (text.offsetHeight > maxHeight) {
-    fontSize--;
+  const div1 = document.getElementById('myDiv1');
+  const text1 = document.getElementById('myText1');
+  const div2 = document.getElementById('myDiv2');
+  const text2 = document.getElementById('myText2');
+
+  const adjustFontSize = (div, text) => {
+    const maxHeight = div.clientHeight;
+    let fontSize = 150; // Initial font size
     text.style.fontSize = fontSize + 'px';
-  }
+
+    while (text.offsetHeight > maxHeight && fontSize > 0) {
+      fontSize--;
+      text.style.fontSize = fontSize + 'px';
+    }
+  };
+
+  adjustFontSize(div1, text1);
+  adjustFontSize(div2, text2);
 }
 
 window.addEventListener('resize', adjustTextSize);
+
+
 
 
 
@@ -26,15 +35,15 @@ let container = document.querySelector('.container')
 // const scrollBar = gsap.to('.scrollbar', { x: () => { return window.innerWidth - (150 + 20) }, ease: "none" })
 
 ScrollTrigger.create({
-    trigger: ".container",
-    start: "top top",
-    // end: () => (container.scrollWidth - window.innerWidth),
-    end: "+=3250",
-    pin: true,
-    anticipatePin: 1,
-    scrub: scrubValue,
-    // animation: scrollBar,
-    invalidateOnRefresh: true,
+  trigger: ".container",
+  start: "top top",
+  // end: () => (container.scrollWidth - window.innerWidth),
+  end: "+=3250",
+  pin: true,
+  anticipatePin: 1,
+  scrub: scrubValue,
+  // animation: scrollBar,
+  invalidateOnRefresh: true,
 })
 
 
@@ -42,41 +51,41 @@ ScrollTrigger.create({
 let thumbNails = gsap.utils.toArray(".thumbnail");
 
 thumbNails.forEach((thumb, i) => {
- 
+
   if (thumb.classList.contains('fakePin')) {
-                
+
     function prevAll(element) {
       var result = [];
 
       while (element = element.previousElementSibling)
-          result.push(element);
+        result.push(element);
       return result;
-    }    
-    
+    }
+
     // console.log(prevAll(thumb))
-    
+
     var totalWidthToMove;
-    
+
     function getTotalWidthToMove() {
-        
+
       totalWidthToMove = 0;
-      
+
       prevAll(thumb).forEach((thumbBefore, i) => {
 
-        let style = thumbBefore.currentStyle || window.getComputedStyle(thumbBefore);      
+        let style = thumbBefore.currentStyle || window.getComputedStyle(thumbBefore);
         let marginRight = parseInt(style.marginRight);
 
-        totalWidthToMove += thumbBefore.offsetWidth - 35;
+        totalWidthToMove += thumbBefore.offsetWidth - 5;
         console.log(totalWidthToMove);
 
-      });    
+      });
 
       return totalWidthToMove;
-      
+
     }
     //getTotalWidthToMove();
     // console.log(totalWidthToMove);
-    
+
     gsap.to(thumb, {
       x: () => { return - getTotalWidthToMove() },
       ease: "none",
@@ -88,10 +97,10 @@ thumbNails.forEach((thumb, i) => {
         end: () => "+=" + getTotalWidthToMove(),
       }
     });
-    
+
   }
   else {
-    
+
     gsap.to(thumb, {
       x: () => { return - (container.scrollWidth) },
       ease: "none",
@@ -103,9 +112,12 @@ thumbNails.forEach((thumb, i) => {
         end: () => "+=" + (container.scrollWidth),
       }
     });
-    
+
   }
-    
+
   //console.log(thumb.offsetWidth)
-  
+
 });
+
+
+
